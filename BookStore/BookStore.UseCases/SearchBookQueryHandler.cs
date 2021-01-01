fa846739cd3590b817f5ab0a6899da721ce18584
smdshahbazalam <76739739+smdshahbazalam@users.noreply.github.com>
2021-01-01 10:32:43 +0530

    Add files via upload

---
+++ BookStore/BookStore.UseCases/SearchBookQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using BookStore.Data.EntityFramework;
+using BookStore.Data.EntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.UseCases
+{
+    public class SearchBookQueryHandler
+    {
+        private readonly BookStoreContext bookStoredbContext;
+
+        public SearchBookQueryHandler(BookStoreContext bookStoredbContext)
+        {
+            this.bookStoredbContext = bookStoredbContext;
+        }
+
+        public async Task<Book[]> FetchAsync(string searchKey)
+        {
+            return await bookStoredbContext.Books
+                .Where(u => EF.Functions.Like(u.Name, $"{searchKey}%")
+                || EF.Functions.Like(u.Author, $"{searchKey}%")).ToArrayAsync();
+        }
+    }
+}
